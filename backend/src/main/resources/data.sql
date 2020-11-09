393c828c52c73185f450d0fbb4fae235ae50d9a8
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-11-09 20:09:53 -0300

    User and Role

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Leonardo Delmiro', 'leonardo@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Nelio Alves', 'nelio@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
