6ec37c28c6a11e6dbaea4f90d9a7bfa4a0e6c091
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-11-09 20:23:41 -0300

    Genre

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -7,3 +7,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+
+INSERT INTO tb_genre (name) VALUES ('Comédia');
+INSERT INTO tb_genre (name) VALUES ('Terror');
+INSERT INTO tb_genre (name) VALUES ('Drama');
