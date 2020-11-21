d1719c7b82f2dc075d797fe91915406ecce95100
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-11-20 21:26:40 -0300

    Implement security and login

---
+++ backend/src/main/java/com/leodelmiro/movieflix/components/JwtTokenEnhancer.java
@@ -0,0 +1,35 @@
+package com.leodelmiro.movieflix.components;
+
+import com.leodelmiro.movieflix.entities.User;
+import com.leodelmiro.movieflix.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+
+        User user = userRepository.findByEmail(authentication.getName());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", user.getId());
+        map.put("userName", user.getName());
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+        token.setAdditionalInformation(map);
+
+        return accessToken;
+    }
+}
--- backend/src/main/java/com/leodelmiro/movieflix/entities/User.java
+++ backend/src/main/java/com/leodelmiro/movieflix/entities/User.java
@@ -1,32 +1,39 @@
 package com.leodelmiro.movieflix.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "tb_user_role",
-            joinColumns = @JoinColumn(name="user_id"),
+            joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     @OneToMany(mappedBy = "user")
     private List<Review> reviews = new ArrayList<>();
 
-    public User(){
-        
+    public User() {
+
     }
 
     public User(Long id, String name, String email, String password) {
@@ -75,6 +82,39 @@ public class User implements Serializable {
     public List<Review> getReviews() {
         return reviews;
     }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 
     @Override
     public boolean equals(Object o) {
